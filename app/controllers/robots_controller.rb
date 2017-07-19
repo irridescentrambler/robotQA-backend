9ac18cd752ee3146337290ee0a922e281aee60f1
irridescentrambler <mohadikar59@gmail.com>
2017-07-19 16:17:41 +0530

    scopes added for robot

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,10 +1,10 @@
 class RobotsController < ApplicationController
   def index
-    @robots_to_extinguish = Robot.where(on_fire: true).all
-    @robots_to_recycle = Robot.where('number_of_rotors<? or number_of_rotors>? or color=? or ( has_wheels=? and has_tracks=? ) or ( has_wheels=? and rusty=? ) or ( has_sentience=? and loose_screws=?) or on_fire=?', 3,8,'blue', true, true, true, true, true, true, true).all - @robots_to_extinguish
-    remaining_robots = Robot.all - @robots_to_extinguish.to_a - @robots_to_service.to_a
-    @robots_for_factory_qa = remaining_robots.to_a - Robot.where('( rusty=? and loose_screws=?) or paint_scratched=?', true, true, true)
-    @robots_for_factory_second = Robot.where('( rusty=? and loose_screws=?) or paint_scratched=?', true, true, true) - remaining_robots
+    @robots_to_extinguish = Robot.where(on_fire: true)
+    @robots_to_recycle = Robot.recycle - @robots_to_extinguish
+    remaining_robots = Robot.all - @robots_to_extinguish.to_a - @robots_to_recycle.to_a
+    @robots_for_factory_qa = Robot.factory_qa.to_a - remaining_robots.to_a
+    @robots_for_factory_second = Robot.factory_second.to_a - remaining_robots.to_a
     respond_to do |format|
       format.json{
         render json: { extinguish: @robots_to_extinguish,
@@ -36,12 +36,17 @@ class RobotsController < ApplicationController
 
   def extinguish
     @robot = Robot.find(params[:id])
-    @robot.is_extinguished = true
-    @robot.save
+
     respond_to do |format|
-      format.json{
-        render json: { message: "#{ @robot.name } is successfully extinguished" }
-      }
+      if @robot.update(is_extinguished: true)
+        format.json {
+          render json: { message: "#{ @robot.name } is successfully extinguished" }
+        }
+      else
+        format.json{
+          render json: { message: 'Error while updating.' }
+        }
+      end
     end
   end
 
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,2 +1,20 @@
 class Robot < ApplicationRecord
+  scope :fire, -> { where(on_fire: true) }
+  scope :rotors_more_than, lambda {|number| where('number_of_rotors>?', number) }
+  scope :rotors_less_than, lambda {|number| where('number_of_rotors<?', number) }
+  scope :color, lambda {|color| where(color: color) }
+
+  scope :recycle, -> { where('number_of_rotors<? or number_of_rotors>? or
+                              color=? or ( has_wheels=? and has_tracks=? ) or
+                              ( has_wheels=? and rusty=? ) or
+                              ( has_sentience=? and loose_screws=?) or
+                              on_fire=?',
+                              3,8,'blue', true, true, true, true, true, true, true)
+                      }
+
+  scope :factory_second, -> { where('( rusty=? and loose_screws=?) or
+                                      paint_scratched=?', true, true, true)
+                            }
+
+  scope :factory_qa, -> { where('( rusty=? and loose_screws=?) or paint_scratched=?', true, true, true) }
 end
